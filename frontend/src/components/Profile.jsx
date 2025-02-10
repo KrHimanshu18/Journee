4bb633cc17154fb6e3402eac7a7c9fda64c3e281
Kr_Himanshu <155032817+KrHimanshu18@users.noreply.github.com>
2025-02-10 11:32:10 +0530

    Profile page updated

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -2,7 +2,7 @@ import { React } from "react";
 
 function Profile() {
   return (
-    <div className="h-screen bg-black">
+    <div className="h-screen bg-[#12202e]">
       <header className="flex justify-between items-center px-8 py-4 bg-[#212529] shadow-md text-white">
         <div className="flex items-center gap-2">
           <svg
@@ -69,64 +69,63 @@ function Profile() {
       </header>
 
       <section className="flex flex-col justify-center">
-        <div className="flex">
-          <div className="flex flex-col">
-            <div className="flex">
+        <div className="flex flex-wrap md:flex-nowrap justify-between items-center p-6 rounded-2xl w-full mx-auto space-y-6 md:space-y-0 my-10">
+          <div className="flex flex-col items-center md:items-start space-y-4">
+            <div className="flex items-center space-x-6">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="60"
                 height="60"
                 viewBox="0 0 24 24"
-                style={{ fill: "rgba(255, 215, 0, 1)" }}
+                className="text-amber-300"
               >
                 <path d="M12 2C6.486 2 2 6.486 2 12c.001 5.515 4.487 10.001 10 10.001 5.514 0 10-4.486 10.001-10.001 0-5.514-4.486-10-10.001-10zm0 18.001c-4.41 0-7.999-3.589-8-8.001 0-4.411 3.589-8 8-8 4.412 0 8.001 3.589 8.001 8-.001 4.412-3.59 8.001-8.001 8.001z"></path>
               </svg>
-              <div className="flex gap-5">
-                <a href="" className="flex flex-col justify-center text-center">
-                  <p className="font-semibold text-center text-white text-xl">
-                    Post
-                  </p>
-                  <p className="font-semibold text-center text-white text-xl">
-                    2
-                  </p>
+              <div className="flex space-x-6 text-center">
+                <a
+                  href="#"
+                  className="flex flex-col hover:text-amber-300 transition"
+                >
+                  <p className="font-semibold text-white text-lg">Post</p>
+                  <p className="font-semibold text-white text-lg">2</p>
                 </a>
-                <a href="" className="flex flex-col justify-center text-center">
-                  <p className="font-semibold text-center text-white text-xl">
-                    Followers
-                  </p>
-                  <p className="font-semibold text-center text-white text-xl">
-                    12
-                  </p>
+                <a
+                  href="#"
+                  className="flex flex-col hover:text-amber-300 transition"
+                >
+                  <p className="font-semibold text-white text-lg">Followers</p>
+                  <p className="font-semibold text-white text-lg">12</p>
                 </a>
-                <a href="" className="flex flex-col justify-center text-center">
-                  <p className="font-semibold text-center text-white text-xl">
-                    Following
-                  </p>
-                  <p className="font-semibold text-center text-white text-xl">
-                    4
-                  </p>
+                <a
+                  href="#"
+                  className="flex flex-col hover:text-amber-300 transition"
+                >
+                  <p className="font-semibold text-white text-lg">Following</p>
+                  <p className="font-semibold text-white text-lg">4</p>
                 </a>
               </div>
             </div>
 
-            <div className="flex flex-col justify-center text-start">
-              <h2 className="font-bold text-amber-300 text-xl">Kr_Himanshu</h2>
-              <p className="font-bold text-xl text-white">
+            <div className="text-center md:text-left">
+              <h2 className="text-amber-300 font-bold text-2xl">
+                @Kr_Himanshu
+              </h2>
+              <p className="text-white font-medium">
                 Web Developer, Creating interactive and responsive web page
-                design
+                designs.
               </p>
             </div>
           </div>
 
-          <div className="flex flex-col justify-center text-start">
-            <h2 className="bg-white text-black font-bold text-xl">
+          <div className="flex flex-col items-center text-center bg-[#d9d9d9] rounded-xl shadow-md w-full max-w-md border-white">
+            <h2 className="bg-white text-black font-bold text-xl py-2 w-full rounded-t-lg">
               I Think...
             </h2>
-            <p className="text-white font-semibold text-xl">
+            <p className="text-white font-semibold text-lg bg-[#12202e]">
               Success is not final, failure is not fatal: it is the courage to
               continue that counts.
             </p>
-            <button className="bg-amber-300 text-black font-bold text-xl">
+            <button className="bg-amber-300 text-black font-bold text-lg px-6 py-2 w-full rounded-b-lg hover:bg-amber-400 transition duration-300">
               Post
             </button>
           </div>
