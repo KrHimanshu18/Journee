9f3082345e1e857a79ef3dfd0824ff6ec3373eb8
Kr_Himanshu <155032817+KrHimanshu18@users.noreply.github.com>
2025-02-26 10:33:06 +0530

    Responsiveness added
    Home and Explore page are completely responsive for all screen sizes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -25,12 +25,12 @@ function Home() {
             height="48"
             viewBox="0 0 24 24"
             style={{ fill: "rgba(255, 215, 0, 1)" }}
-            className="w-[30px] h-[30px] sm:w-[40px] sm:h-[40px]"
+            className="w-[40px] h-[40px]"
           >
             <path d="M6.012 18H21V4a2 2 0 0 0-2-2H6c-1.206 0-3 .799-3 3v14c0 2.201 1.794 3 3 3h15v-2H6.012C5.55 19.988 5 19.805 5 19s.55-.988 1.012-1zM8 6h9v2H8V6z"></path>
           </svg>
           <h1
-            className="text-3xl md:text-4xl"
+            className="text-2xl md:text-4xl"
             style={{
               fontFamily: "'Jacques Francois Shadow', cursive",
               color: "rgba(255, 215, 0, 1)",
